902be2f0a0a79569002245ab79e1db39eb810868
Miki Kiki <krawczykmichal1@gmail.com>
2023-07-07 09:50:33 +0100

    Fix nav and add opacity to logo

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 window.addEventListener("scroll", function() {
     const nav = document.querySelector(".nav-container");
     const video = document.querySelector(".bg-video");
-    const menuHam = document.querySelector('.main-nav')
+    const menuHam = document.querySelector('.main-nav');
     // Change navigation background on scroll
     if (window.scrollY > 50) {
-        menuHam.classList.add("scrolled")
+        menuHam.classList.add("scrolled");
         nav.classList.add("scrolled");
     } else {
-        menuHam.classList.remove("scrolled")
+        menuHam.classList.remove("scrolled");
         nav.classList.remove("scrolled");
     }
 
@@ -16,7 +16,6 @@ window.addEventListener("scroll", function() {
     video.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
 });
 
-
 // get the current year
 const currentYear = new Date().getFullYear();
 
@@ -24,17 +23,16 @@ const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 
 // Hamburger menu
-
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.querySelector('.toggle-btn');
-    
+    const menu = document.querySelector('.main-nav');
+    const menuIcon = document.querySelector('.menu-icon');
+    const closeIcon = document.querySelector('.close-icon');
+    const navContainer = document.querySelector('.nav-container');
+
     toggleButton.addEventListener('click', toggleNavMenu);
 
     function toggleNavMenu() {
-        const menu = document.querySelector('.main-nav');
-        const menuIcon = document.querySelector('.menu-icon');
-        const closeIcon = document.querySelector('.close-icon');
-        const navContainer = document.querySelector('.nav-container')
         navContainer.style.backgroundColor = '';
 
         if (menu.style.display === 'flex') {
@@ -45,11 +43,23 @@ document.addEventListener("DOMContentLoaded", function() {
             menu.style.display = 'flex';
             menuIcon.style.display = 'none';
             closeIcon.style.display = 'block';
-            navContainer.style.backgroundColor = 'rgba(107, 66, 38, .9)'
+            navContainer.style.backgroundColor = 'rgba(107, 66, 38, .9)';
         }
     }
+
+    // Listen for window resize event
+    window.addEventListener('resize', function() {
+        // If the screen size is larger than 768px, reset menu to default state
+        if (window.innerWidth > 768) {
+            menu.style.display = ''; // Reset to default
+            menuIcon.style.display = 'none'; // Hide menu icon
+            closeIcon.style.display = 'none'; // Hide close icon
+            navContainer.style.backgroundColor = ''; // Reset to default
+        } else {
+            menu.style.display = 'none'; // Hide the menu
+            menuIcon.style.display = 'block'; // Show menu icon
+            closeIcon.style.display = 'none'; // Hide close icon
+        }
+    });
+
 });
-
-
-  
-  
