6c2853074cea254b44288a2326d24510c14195f4
Miki Kiki <krawczykmichal1@gmail.com>
2023-07-07 08:53:37 +0100

    complete styling for hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 window.addEventListener("scroll", function() {
     const nav = document.querySelector(".nav-container");
     const video = document.querySelector(".bg-video");
-
+    const menuHam = document.querySelector('.main-nav')
     // Change navigation background on scroll
     if (window.scrollY > 50) {
+        menuHam.classList.add("scrolled")
         nav.classList.add("scrolled");
     } else {
+        menuHam.classList.remove("scrolled")
         nav.classList.remove("scrolled");
     }
 
@@ -32,15 +34,18 @@ document.addEventListener("DOMContentLoaded", function() {
         const menu = document.querySelector('.main-nav');
         const menuIcon = document.querySelector('.menu-icon');
         const closeIcon = document.querySelector('.close-icon');
+        const navContainer = document.querySelector('.nav-container')
+        navContainer.style.backgroundColor = '';
 
-        if (menu.style.display === 'block') {
+        if (menu.style.display === 'flex') {
             menu.style.display = 'none';
             menuIcon.style.display = 'block';
             closeIcon.style.display = 'none';
         } else {
-            menu.style.display = 'block';
+            menu.style.display = 'flex';
             menuIcon.style.display = 'none';
             closeIcon.style.display = 'block';
+            navContainer.style.backgroundColor = 'rgba(107, 66, 38, .9)'
         }
     }
 });
