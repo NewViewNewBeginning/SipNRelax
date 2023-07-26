fec930f28487795f53eb6282529711979d47d753
Miki Kiki <krawczykmichal1@gmail.com>
2023-07-26 10:24:07 +0100

    update php

--- form_process.php
+++ form_process.php
@@ -1,14 +1,43 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = $_POST['first-name'];
-    $lastName = $_POST['last-name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    // Sanitize and validate first name
+    $firstName = filter_input(INPUT_POST, 'first-name', FILTER_SANITIZE_STRING);
+    if (empty($firstName)) {
+        die('Error: First name is required');
+    }
 
-    // email structure
-    $to = "....@gmail.com";
+    // Sanitize and validate last name
+    $lastName = filter_input(INPUT_POST, 'last-name', FILTER_SANITIZE_STRING);
+    if (empty($lastName)) {
+        die('Error: Last name is required');
+    }
+
+    // Sanitize and validate email
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die('Error: Invalid email format');
+    }
+
+    // Sanitize and validate phone
+    $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+    if (empty($phone)) {
+        die('Error: Phone number is required');
+    }
+
+    // Sanitize and validate subject
+    $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+    if (empty($subject)) {
+        die('Error: Subject is required');
+    }
+
+    // Sanitize and validate message
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    if (empty($message)) {
+        die('Error: Message is required');
+    }
+
+    // Email structure
+    $to = "michalkrawczyk666@wp.pl";
     $headers = "From: $email\r\n";
     $email_subject = "New form submission: $subject";
     $email_body = "You have received a new message from $firstName $lastName.\n".
@@ -18,14 +47,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     "Email: $email\n".
                     "Phone: $phone\n".
                     "Message:\n$message";
-    
-    // use wordwrap() if lines are longer than 70 characters
+
+    // Use wordwrap() if lines are longer than 70 characters
     $email_body = wordwrap($email_body, 70, "\r\n");
 
-    // send the email
-    mail($to, $email_subject, $email_body, $headers);
+    // Send the email
+    if(!mail($to, $email_subject, $email_body, $headers)) {
+        die('Error: Unable to send email. Please try again later.');
+    }
 
-    // redirect to a thank you page
-    header("Location: thankyou.html");
+    // Redirect to a thank you page
+    header("Location: /html/thank-you.html");
 }
 ?>
